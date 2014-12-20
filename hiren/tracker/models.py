d7e78924e625a00469ea714a35df39b10882ea44
prism <git.pyprism@gmail.com>
2014-12-20 23:20:02 +0600

    added new field

--- hiren/tracker/models.py
+++ hiren/tracker/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Hiren(models.Model):
+    user = models.ForeignKey(User)
     tag = TaggableManager()
     amount = models.FloatField()
     description = models.TextField(blank=True, null=True)
