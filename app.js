af305d1e4c8efdff98fc3925a8b19d24ae4f306b
Ashutosh Das <git.pyprism@gmail.com>
2015-06-23 23:38:42 +0600

    basic settings

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by prism on 6/22/15.
+ */
+
+var express = require('express'),
+    mongoose = require('mongoose'),
+    manager = require('./models/manager');
+
+var app = express(),
+    router = express.Router();
+
+var port = process.env.PORT || 4000,
+    db = mongoose.connect( process.env.DB || 'mongodb://localhost/hiren_expense');
+
+
+router.route('/expense')
+    .get(function(req, res){
+       manager.Tag.find(function(err, name) {
+           if(err)
+                res.status(500).send(err);
+           else
+               res.json(name);
+       })
+    });
+
+app.enable('trust proxy');
+app.use('/api',router);
+
+app.get('/', function(req, res){
+    res.send('test');
+});
+
+app.listen(port, function(){
+    console.log('App is running on port: ' + port);
+});
