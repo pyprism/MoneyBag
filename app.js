450c0210ff95dee072de9c9ad73589efe6c2c9b3
Ashutosh Das <git.pyprism@gmail.com>
2015-06-26 18:17:05 +0600

    added initial amount controller's require

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var express = require('express'),
     manager = require('./models/manager'),
     bodyParser = require('body-parser');
 
-incomeRoute = require('./routes/income')(manager);  // injecting manager model :D impressive isn't it ?
-
+incomeRoute = require('./routes/income')(manager);  // injecting manager model(s) :D impressive isn't it ?
+initialAmount = require('./routes/initial')(manager);
 
 var app = express();
 
@@ -17,6 +17,7 @@ app.enable('trust proxy');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/api/income',incomeRoute);
+app.use('/api/initial',initialAmount);
 
 var port = process.env.PORT || 4000,
     db = mongoose.connect( process.env.DB || 'mongodb://localhost/hiren_expense');
