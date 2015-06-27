02fa014257b2d13ba279fcf184639417a8d77be8
Ashutosh Das <git.pyprism@gmail.com>
2015-06-28 00:29:11 +0600

    added new router

---
+++ routes/loan.js
@@ -0,0 +1,27 @@
+/**
+ * Created by prism on 6/27/15.
+ */
+var express = require('express');
+
+var routes = function(manager){
+    var router = express.Router();
+
+    router.route('/')
+        .get(function(req, res) {
+            manager.Loan.find(function(err, data){
+                if(err)
+                    res.status(500).send(err);
+                res.json(data);
+            });
+        })
+        .post(function(req, res){
+
+            var hiren = new manager.Loan(req.body);
+            hiren.save(function(err) {
+                if (err)
+                  res.status(409).send(err);
+            res.status(201).send(hiren);
+        })
+};
+
+module.export = routes;
