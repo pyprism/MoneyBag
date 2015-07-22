7e7174f69ff05859df650d4c435e90ebc80fe3ec
Ashutosh Das <git.pyprism@gmail.com>
2015-07-22 20:11:33 +0600

    Added authentication middleware

--- app.js
+++ app.js
@@ -5,13 +5,42 @@
 var express = require('express'),
     mongoose = require('mongoose'),
     manager = require('./models/manager'),
-    bodyParser = require('body-parser');
+    user = require('./models/users'),
+    bodyParser = require('body-parser'),
+    jwt = require('jwt-simple'),
+    moment = require('moment');
 
 incomeRoute = require('./routes/income')(manager);  // injecting manager model(s) :D impressive isn't it ?
 initialAmount = require('./routes/initial')(manager);
 loan = require('./routes/loan')(manager);
 
 var app = express();
+
+/*
+ |--------------------------------------------------------------------------
+ | Login Required Middleware
+ |--------------------------------------------------------------------------
+ */
+function ensureAuthenticated(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+    }
+    var token = req.headers.authorization.split(' ')[1];
+
+    var payload = null;
+    try {
+        payload = jwt.decode(token, "some secret strings ? ;) , what about Hiren ? :P hehe ");
+    }
+    catch (err) {
+        return res.status(401).send({ message: err.message });
+    }
+
+    if (payload.exp <= moment().unix()) {
+        return res.status(401).send({ message: 'Token has expired' });
+    }
+    req.user = payload.sub;
+    next();
+}
 
 
 app.enable('trust proxy');
@@ -22,7 +51,8 @@ app.use('/api/initial',initialAmount);
 app.use('/api/loan',loan);
 
 var port = process.env.PORT || 4000,
-    db = mongoose.connect( process.env.DB || 'mongodb://localhost/hiren_expense');
+    db = mongoose.connect( process.env.DB || 'mongodb://localhost/hiren_expense'),
+    TOKEN_SECRET = process.env.TOKEN_SECRET || 'A hard to guess string';
 
 
 app.get('/', function(req, res){
