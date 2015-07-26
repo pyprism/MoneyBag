b9025902f6e0428224e19bb060c9400a488ae690
Ashutosh Das <git.pyprism@gmail.com>
2015-07-26 08:50:51 +0600

    .

--- app.js
+++ app.js
@@ -10,9 +10,12 @@ var express = require('express'),
     jwt = require('jwt-simple'),
     moment = require('moment');
 
+
+
 incomeRoute = require('./routes/income')(manager);  // injecting manager model(s) :D impressive isn't it ?
 initialAmount = require('./routes/initial')(manager);
 loan = require('./routes/loan')(manager);
+auth = require('./routes/user')(user, createJWT);
 
 var app = express();
 
@@ -29,7 +32,7 @@ function ensureAuthenticated(req, res, next) {
 
     var payload = null;
     try {
-        payload = jwt.decode(token, "some secret strings ? ;) , what about Hiren ? :P hehe ");
+        payload = jwt.decode(token, TOKEN_SECRET);
     }
     catch (err) {
         return res.status(401).send({ message: err.message });
@@ -42,6 +45,19 @@ function ensureAuthenticated(req, res, next) {
     next();
 }
 
+/*
+ |--------------------------------------------------------------------------
+ | Generate JSON Web Token
+ |--------------------------------------------------------------------------
+ */
+function createJWT(user) {
+    var payload = {
+        sub: user._id,
+        iat: moment().unix(),
+        exp: moment().add(14, 'days').unix()
+    };
+    return jwt.encode(payload, TOKEN_SECRET);
+}
 
 app.enable('trust proxy');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -49,10 +65,11 @@ app.use(bodyParser.json());
 app.use('/api/income',incomeRoute);
 app.use('/api/initial',initialAmount);
 app.use('/api/loan',loan);
+app.use('/api/auth', auth);
 
 var port = process.env.PORT || 4000,
     db = mongoose.connect( process.env.DB || 'mongodb://localhost/hiren_expense'),
-    TOKEN_SECRET = process.env.TOKEN_SECRET || 'A hard to guess string';
+    TOKEN_SECRET = process.env.TOKEN_SECRET || "some secret strings ? ;) , what about Hiren ? :P hehe ";
 
 
 app.get('/', function(req, res){
