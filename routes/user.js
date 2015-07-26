d7056b0a00821c2f3e142d425f94bbcb16813cf2
Ashutosh Das <git.pyprism@gmail.com>
2015-07-26 08:51:07 +0600

    .

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,46 @@
  * Created by prism on 7/21/15.
  */
 var express = require('express');
-var router = express.Router();
 
 
-router.route('/')
-    .get()
+
+var routes = function(User, createJWT) {
+    var router = express.Router();
+
+    router.route('/signup')
+        .post(function( req, res) {
+            User.findOne({email: req.body.email }, function(err, existingUser) {
+                if (existingUser) {
+                    return res.status(409).json({ "message": "Email is already taken"});
+                }
+
+                var user = new User({
+                    email: req.body.email,
+                    password: req.body.password
+                });
+                user.save(function() {
+                    res.json({ "token": createJWT(user)});
+                });
+            });
+        });
+
+    router.route('/login')
+        .post(function(req, res) {
+            User.findOne({ email: req.body.email }), '+password', function(err, user) {
+                if (!user) {
+                    return res.status(401).json({ 'message': 'Wrong email and/or password'})
+                }
+            }
+
+            User.comparePassword(req.body.password, function(err, isMatch) {
+                if(!isMatch) {
+                    return res.status(401).json({ 'message': 'Wrong email or/and password'})
+                }
+                res.json({ 'token': createJWT(user)});
+            });
+        });
+
+    return router;
+};
+
+module.exports = routes;
