c067297cb2daa89d26e03159d0ba11c1d49ff52e
Ashutosh Das <git.pyprism@gmail.com>
2015-11-11 20:57:28 +0600

    update

--- bunny/views.py
+++ bunny/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import permissions, viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Account
+from .permissions import IsAccountOwner
+from .serializers import AccountSerializer
 
-# Create your views here.
+
+class AccountViewSet(viewsets.ModelViewSet):
+    lookup_field = 'username'
+    queryset = Account.objects.all()
+    serializer_class = AccountSerializer
+
+    def get_permissions(self):
+        if self.request.method in permissions.SAFE_METHODS:
+            return (permissions.AllowAny(),)
+
+        if self.request.method == 'POST':
+            return (permissions.AllowAny(),)
+
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            Account.objects.create_user(**serializer.validated_data)
+
+            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+
+        return Response({
+            'status': 'Bad request',
+            'message': 'Account could not be created with received data.'
+        }, status=status.HTTP_400_BAD_REQUEST)
