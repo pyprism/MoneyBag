a683015128acc201537806eb48a09845dad3f09d
Ashutosh Das <git.pyprism@gmail.com>
2015-11-12 22:21:13 +0600

    add route

--- hiren/urls.py
+++ hiren/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from rest_framework import routers
+from bunny.views import AccountViewSet
+from bunny import views
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/', include(router.urls)),
+    url('^.*$', views.index, name='index'),
 ]
