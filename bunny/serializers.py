7f79bddb9f1b2416ba70acc638f1fdfb41a4afcf
Ashutosh Das <git.pyprism@gmail.com>
2015-11-12 22:20:51 +0600

    change permission

--- bunny/serializers.py
+++ bunny/serializers.py
@@ -7,6 +7,7 @@ from .models import Account
 class AccountSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
     confirm_password = serializers.CharField(write_only=True, required=False)
+    username = serializers.CharField(write_only=True)
 
     class Meta:
         model = Account
