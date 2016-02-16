5c490440f69d02a2b37116e4467f676131017cf9
Ashutosh Das <git.pyprism@gmail.com>
2016-02-16 22:47:46 +0600

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var debug = process.env.NODE_ENV !== "production",
+    webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + "/public/src",
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./js/client.js",
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015'],
+                    plugins: ['transform-class-properties', "react-html-attrs"]
+                }
+            }
+        ]
+    },
+    output: {
+        path: __dirname + "/public/",
+        filename: "client.min.js"
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false})
+    ]
+};
