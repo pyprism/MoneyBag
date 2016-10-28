430e704de6782ffd13ca7949eff8603a63452648
Ashutosh Das <git.pyprism@gmail.com>
2016-10-28 23:36:21 +0600

    added some permission to account model

--- bunny/models.py
+++ bunny/models.py
@@ -32,6 +32,30 @@ class Account(AbstractBaseUser):
 
     objects = AccountManager()
 
+    def get_full_name(self):
+        # The user is identified by their username
+        return self.username
+
+    def get_short_name(self):
+        # The user is identified by their username
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
 
 class Budget(models.Model):
     month = models.DateField()
