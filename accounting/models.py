d2c3405097227d2cf3b052d81fe9209536772a80
Ashutosh Das <git.pyprism@gmail.com>
2017-07-01 13:29:33 +0600

    wip

--- accounting/models.py
+++ accounting/models.py
@@ -4,11 +4,20 @@ from django.contrib.auth.models import User
 
 class AccountHead(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    parent_id = models.ForeignKey('AccountHead', on_delete=models.CASCADE)
-    name = models.CharField(max_length=400, null=True)
-    type = models.CharField(max_length=400, null=True)
+    parent_id = models.ForeignKey('AccountHead', on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=400, null=True, blank=True)
+    type = models.CharField(max_length=400, null=True, blank=True)
     head_code = models.IntegerField(null=True)
-    ledger_head_code = models.CharField(max_length=255, null=True)
-    # created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    ledger_head_code = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transaction_date = models.DateTimeField(null=True, blank=True)
+    transaction_ref = models.ForeignKey('Transaction', null=True, blank=True)
+    voucher_type = models.IntegerField(null=True, blank=True)
+    voucher_number = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
