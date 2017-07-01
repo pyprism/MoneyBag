8a7ed09d659a158efe4674ca99fe690f013e6290
Ashutosh Das <git.pyprism@gmail.com>
2017-07-01 14:46:12 +0600

    fixed model

--- accounting/models.py
+++ accounting/models.py
@@ -10,12 +10,12 @@ class AccountHead(models.Model):
         ("exp", 'expense'),
         ("inc", 'income'),
     )
-    user = models.ForeignKey(User)
-    parent_id = models.ForeignKey('AccountHead', null=True, blank=True)
-    name = models.CharField(max_length=400, null=True, blank=True)
-    type = models.CharField(choices=HEAD_TYPES, max_length=5, null=True, blank=True)
-    head_code = models.IntegerField(null=True)
-    ledger_head_code = models.CharField(max_length=255, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_id = models.ForeignKey('AccountHead', null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=400, blank=True)
+    type = models.CharField(choices=HEAD_TYPES, max_length=5, blank=True)
+    head_code = models.IntegerField(null=True, blank=True)
+    ledger_head_code = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -42,24 +42,28 @@ class Transaction(models.Model):
         (8, 'Credit note'),
     )
 
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     transaction_date = models.DateTimeField(null=True, blank=True)
-    transaction_ref = models.ForeignKey('Transaction', null=True, blank=True)
+    transaction_ref = models.ForeignKey('Transaction', null=True, blank=True, on_delete=models.CASCADE)
     voucher_type = models.SmallIntegerField(choices=VOUCHER_TYPES)
-    voucher_number = models.CharField(max_length=255, null=True, blank=True)
+    voucher_number = models.CharField(max_length=255, blank=True)
     voucher_status = models.SmallIntegerField(choices=STATUS_TYPES)
-    description = models.TextField(null=True, blank=True)
+    description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 class TransactionDetails(models.Model):
     TRANS_POSITION = (
         ("dr", 'debit'),
         ("cr", 'credit'),
     )
-    transaction = models.ForeignKey('Transaction')
-    account_head = models.ForeignKey('AccountHead')
+    transaction = models.ForeignKey('Transaction', on_delete=models.CASCADE)
+    account_head = models.ForeignKey('AccountHead', on_delete=models.CASCADE)
     position = models.CharField(choices=TRANS_POSITION, max_length=5)
     amount = models.DecimalField(max_digits=18, decimal_places=3)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'accounting_transaction_details'
