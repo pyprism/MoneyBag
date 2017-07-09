3df290be4e3b52d60ba844840fb26cd04034089d
Ashutosh Das <git.pyprism@gmail.com>
2017-07-09 19:43:21 +0600

    added register url

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from base import views
 
 urlpatterns = [
-    url(r'^', views.login, name='login'),
+    url(r'^$', views.login, name='login'),
+    url(r'^register/', views.register, name='register'),
 ]
